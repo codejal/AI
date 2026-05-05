a40d92b7eebe8141359a068aa3656eabc3ea985f
Pranjal <pranjal@Pranjals-MacBook-Pro.local>
2026-05-05 15:03:22 +0530

    add langsmith observability for our rag pipeline

--- apps/api/src/api/agents/retrieval_generation.py
+++ apps/api/src/api/agents/retrieval_generation.py
@@ -1,21 +1,37 @@
 from dotenv import load_dotenv
 import os
 import voyageai
+from langsmith import traceable, get_current_run_tree
 
 from qdrant_client import QdrantClient
 import anthropic
 
 
-
+@traceable(
+        name="embed_query",
+        run_type="embedding",
+        metadata={"ls_provider": "voyageai", "ls_model_name": "voyage-3"},
+)
 def get_embedding(voyageai_client, text, model = 'voyage-3'):
         result = voyageai_client.embed(
                 [text],
                 model=model,
                 input_type="document"
         )
+
+        # add metadata to langsmith obervability
+        current_run = get_current_run_tree()
+        if current_run:
+                current_run.metadata["usage_metadata"] = {
+                        "total_tokens": result.__dict__['total_tokens'],
+                }
+
         return result.embeddings[0]
 
-
+@traceable(
+        name="retrieve_data",
+        run_type="retriever",
+)
 def retrieve_data(voyageai_client, query, qdrant_client, k=5):
         query_embedding = get_embedding(voyageai_client, query)
         results = qdrant_client.query_points(
@@ -42,14 +58,20 @@ def retrieve_data(voyageai_client, query, qdrant_client, k=5):
                 "similarity_scores": similarity_scores,
         }
 
-
+@traceable(
+        name="format_retrieved_context",
+        run_type="prompt",
+)
 def process_context(context):
         formatted_context = ""
         for id, chunk, rating in zip(context['retrieved_context_ids'], context['retrieved_context'], context['retrieved_context_ratings']):
                 formatted_context += f"- ID: {id}, rating: {rating}, description: {chunk}\n"
         return formatted_context
 
-
+@traceable(
+        name="build_pompt",
+        run_type="prompt",
+)
 def build_pompt(preprocessed_context, question):
         prompt = f"""
 You are a shopping assistant that can answer questions about the products in stock.
@@ -67,7 +89,11 @@ Question:
         """
         return prompt
 
-
+@traceable(
+        name="generate_answer",
+        run_type="llm",
+        metadata={"ls_provider": "anthropic", "ls_model_name": "claude-haiku-4-5"},
+)
 def generate_answer(anthropic_client, prompt):
         message = anthropic_client.messages.create(
                 max_tokens=2000,
@@ -77,11 +103,20 @@ def generate_answer(anthropic_client, prompt):
                         "content": prompt,
                         }
                 ],
-                model="claude-opus-4-7",
+                model="claude-haiku-4-5",
         )
+
+        current_run = get_current_run_tree()
+        if current_run:
+                current_run.metadata["usage_metadata"] = {
+                        "input_tokens": message.__dict__['usage'].__dict__['input_tokens'],
+                        "output_tokens": message.__dict__['usage'].__dict__['output_tokens'],
+                }
         return message.content[0].text
 
-
+@traceable(
+        name="rag_pipeline"
+)
 def rag_pipeline(question, top_k=10):
         load_dotenv()
         VOYAGE_API_KEY = os.environ.get("VOYAGE_API_KEY")
@@ -93,4 +128,13 @@ def rag_pipeline(question, top_k=10):
         prompt = build_pompt(preprocessed_context, question)
         answer = generate_answer(anthropic_client, prompt)
 
-        return answer
+        # for evaluation we should return the following
+        final_result = {
+                "answer": answer,
+                "question": question,
+                "retrieved_context_ids": retrieved_context["retrieved_context_ids"],
+                "retrieved_context": retrieved_context["retrieved_context"],
+                "similarity_scores": retrieved_context["similarity_scores"]
+        }
+
+        return final_result
--- apps/api/src/api/api/endpoints.py
+++ apps/api/src/api/api/endpoints.py
@@ -28,7 +28,7 @@ def rag(
 
     return RAGResponse(
         request_id=request.state.request_id,
-        answer=answer,
+        answer=answer["answer"],
         )
 
 api_router = APIRouter()
